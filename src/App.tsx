5f24e4217fa0f390b2cfbc7dc5884ea3bcf02508
VladTca <votsats@gmail.com>
2024-03-31 13:19:59 +0200

    feat: Add Footer component to App.tsx file
    Added the Footer component to the App.tsx file for better layout structure.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import './App.css';
 import {Header} from "./layout/header/Header";
 import {Main} from "./layout/sections/main/Main";
+import {Footer} from "./layout/footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header/>
         <Main/>
+        <Footer/>
     </div>
   );
 }
