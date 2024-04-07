2679a31e93ee00ee4c7fc0e7f07c45f1e098497a
VladTca <votsats@gmail.com>
2024-04-07 10:05:24 +0200

    Added main.yml workflow file, Container component, and updated Global styles and Header component.

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,37 +2,43 @@ import React from 'react';
 import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo";
 import {Menu} from "../../components/menu/Menu";
+import {Container} from "../../components/Container";
+import {FlexWrapper} from "../../components/Flexwrapper";
 
 
-const items:{menu:string,aKey:string,href:string}[] = [
-    {menu:"Home",aKey:'h',href:"#home"},
-    {menu:"Skills",aKey:'s',href:"#skills"},
-    {menu:"Projects",aKey:'p',href:"#projects"},
-    {menu:"Achievements",aKey:'a',href:"#achievements"},
-    {menu:"Contacts",aKey:'c',href:"#contacts"}
+const items: { menu: string, aKey: string, href: string }[] = [
+    {menu: "Home", aKey: 'h', href: "#home"},
+    {menu: "Skills", aKey: 's', href: "#skills"},
+    {menu: "Projects", aKey: 'p', href: "#projects"},
+    {menu: "Achievements", aKey: 'a', href: "#achievements"},
+    {menu: "Contacts", aKey: 'c', href: "#contacts"}
 
 ]
- export const Header = () => {
+export const Header = () => {
     return (
         <StyledHeader>
-            <Logo />
-            <Menu menuItems={items} />
+            <Container>
+                <FlexWrapper justify={'space-between'} align={'center'} wrap>
+                    <Logo/>
+                    <Menu menuItems={items}/>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
 
-const StyledHeader=styled.header`
+const StyledHeader = styled.header`
     position: fixed;
     top: 0;
     width: 100%;
-   margin-bottom: 60px;
+    margin-bottom: 60px;
     background-color: #343D68;
     display: flex;
     justify-content: space-between;
-    
+
     align-items: center;
     height: 60px;
     overflow: hidden;
-   
-   
+
+
 `
